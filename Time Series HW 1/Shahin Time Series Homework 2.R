868a262e4816111976818a3d346e0767fe2d9d98
SufyanShahin <54315657+SufyanShahin@users.noreply.github.com>
2019-09-08 19:10:28 -0400

    Create Shahin Time Series Homework 2.R

---
+++ Time Series HW 1/Shahin Time Series Homework 2.R
@@ -0,0 +1,88 @@
+#------------------------------------#
+#        Exponential Smoothing       #
+#               Models               #
+#                                    #
+#           Dr Susan Simmons         #
+#------------------------------------#
+
+# Needed Libraries for Analysis #
+install.packages('lubridate')
+library(haven)
+library(forecast)
+library(fma)
+library(tseries)
+library(expsmooth)
+library(lmtest)
+library(zoo)
+library(ggplot2)
+library(lubridate)
+library(plyr)
+
+# Saving File Locations and Uploading SAS File #
+file.dir <- "C:\\Users\\Asus\\Documents\\Learning\\Advanced Analytics\\Time Series and Forecasting\\Homework\\Homework 2\\PM_2_5_Raleigh2.csv"
+hw2 <- read.csv(file.dir)
+
+View(hw2)
+
+#Aggregating the data by month
+hw2$Date2 <- as.Date(hw2$Date, "%m/%d/%Y")
+mo <- strftime(hw2$Date2, "%m")
+yr <- strftime(hw2$Date2, "%Y")
+dd <- data.frame(mo, yr, hw2$Daily.Mean.PM2.5.Concentration)
+
+View(dd)
+
+dd.agg <- aggregate(hw2.Daily.Mean.PM2.5.Concentration ~ mo + yr, dd, FUN = mean)
+dd.agg$date <- as.POSIXct(paste(dd.agg$yr, dd.agg$mo, "01", sep = "-"))
+
+View(dd.agg)
+
+
+#Split the data
+data.train <- dd.agg[dd.agg$date <= "2018-06-1",]
+
+data.valid <- dd.agg[dd.agg$date > "2018-06-01",]
+
+View(data.train)
+
+# Creating Time Series Data Objects #
+Daily_Mean_Train <- ts(data.train$hw2.Daily.Mean.PM2.5.Concentration, start = 2014, frequency = 12)
+
+Daily_Mean_Valid <- ts(data.valid$hw2.Daily.Mean.PM2.5.Concentration, start = 2018, frequency = 12)
+
+# STL Decomposition
+decomp_stl <- stl(Daily_Mean_Train, s.window = 7)
+plot(decomp_stl)
+
+# Plot of the data with the trend line, STL
+plot(Daily_Mean_Train, col = "grey", main = "PM2.5 - Trend/Cycle", xlab = "", ylab = "PM2.5", lwd = 2)
+lines(decomp_stl$time.series[,2], col = "red", lwd = 2)
+
+# Plot of the data with the seasonally adjusted overlay, STL
+seas_pass = Daily_Mean_Train - decomp_stl$time.series[,1]
+plot(Daily_Mean_Train, col = "grey", main = "PM2.5 - Seasonally Adjusted", xlab = "", ylab = "PM2.5", lwd = 2)
+lines(seas_pass, col = "red", lwd = 2)
+
+# Month plot. Cool stuff!
+monthplot(decomp_stl$time.series[,"seasonal"], main = "PM2.5 - Monthly Effects", ylab = "Seasonal Sub Series", xlab = "Seasons (Months)", lwd = 2)
+
+
+# Building a Single Exponential Smoothing Model - Steel Data #
+SES.Train <- ses(Daily_Mean_Train, initial = "optimal", h = 0)
+summary(SES.Train)
+
+plot(SES.Train, main = "Mean PM2.5 Simple ESM Forecast", xlab = "Date", ylab = "PM2.5")
+round(accuracy(SES.Train),2)
+
+autoplot(SES.Train)+
+  autolayer(fitted(SES.Train),series="Fitted")+ylab("PM2.5 with Holt-Winters ESM Forecast")
+
+
+#Holt-Winters ESM
+HWES.PM <- hw(Daily_Mean_Train, seasonal = "additive")
+summary(HWES.PM)
+
+plot(HWES.PM, main = "PM2.5 with Holt-Winters ESM Forecast", xlab = "Date", ylab = "PM2.5")
+
+autoplot(HWES.PM)+
+  autolayer(fitted(HWES.PM),series="Fitted")+ylab("PM2.5 with Holt-Winters ESM Forecast")
