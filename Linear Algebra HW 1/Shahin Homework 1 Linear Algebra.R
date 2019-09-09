c443f69fa14923878da8e0f1f511303ddd718eb7
SufyanShahin <54315657+SufyanShahin@users.noreply.github.com>
2019-09-09 07:30:48 -0400

    Create Shahin Homework 1 Linear Algebra.R

---
+++ Linear Algebra HW 1/Shahin Homework 1 Linear Algebra.R
@@ -0,0 +1,25 @@
+#Load in R data table
+
+load('C:/Users/Asus/Documents/Learning/Advanced Analytics/Linear Algebra/Homework/Homework 1/LeukError.RData')
+table(leuk$V5001)
+
+View(leuk)
+
+#Cool library
+
+library(RColorBrewer)
+display.brewer.all()
+
+#Make a scatterplot that's less cool
+
+randomColumns = sample(1:5000,2)
+plot(leuk[,randomColumns],col = leuk$V5001)
+
+
+#Create the first two principal components
+
+pcaOut = prcomp(leuk[,1:5000],3, scale = F)
+plot(pcaOut$x[,1],pcaOut$x[,2],col = leuk$V5001, xlab = "Principal Component 1", ylab = "Principal Component 2", main = 'Genetic Samples Projected into 2-dimensions')
+
+# Observation 22 is misclassified as...green?
+text(pcaOut$x[,1], pcaOut$x[,2], labels=(as.numeric(rownames(leuk))), data=leuk, cex=0.5, font=2)
