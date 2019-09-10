01bcaa0b1eef95be6cb320207890c5c9605c5cf7
SufyanShahin <54315657+SufyanShahin@users.noreply.github.com>
2019-09-10 19:38:16 -0400

    Update Shahin Homework 1 Linear Algebra.R

--- Linear Algebra HW 1/Shahin Homework 1 Linear Algebra.R
+++ Linear Algebra HW 1/Shahin Homework 1 Linear Algebra.R
@@ -19,9 +19,9 @@ plot(leuk[,randomColumns],col = leuk$V5001)
 #Create the first two principal components
 
 pcaOut = prcomp(leuk[,1:5000],3, scale = F)
-plot(pcaOut$x[,1],pcaOut$x[,2],col = leuk$V5001, xlab = "Principal Component 1", ylab = "Principal Component 2", main = 'Genetic Samples Projected into 2-dimensions')
+plot(pcaOut$x[,1],pcaOut$x[,2],col = leuk$V5001, cex = 2, xlab = "Principal Component 1", ylab = "Principal Component 2", main = 'Genetic Samples Projected into 2-dimensions')
 
 # Observation 19 is misclassified a AML(Green). Should be ALL-T (Red).
-text(pcaOut$x[,1], pcaOut$x[,2], labels=(as.numeric(rownames(leuk))), data=leuk, cex=0.5, font=2)
+text(pcaOut$x[,1], pcaOut$x[,2], labels=(as.numeric(rownames(leuk))), data=leuk, cex=0.7, font=2)
 
-legend(40000, -13000, legend = c("AML", "ALL-T", "ALL_B"), col=c("green", "red", "black"), lty=1:2, cex=0.7)
+legend(40000, -13000, legend = c("AML", "ALL-T", "ALL_B"), col=c("green", "red", "black"), lty=1:2, cex=0.7, pch = 1)
